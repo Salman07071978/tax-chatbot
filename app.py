d94c4aab5f4adff7a83219655971308b13d55919
Salman07071978 <salman.hafeezca@gmail.com>
2026-06-04 01:27:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.title("Pakistan Tax Chatbot")
+
+st.write("Ask Income Tax or Sales Tax related questions")
+
+question = st.text_input("Your Question")
+
+if question:
+    st.write("You asked:", question)
+    st.info("Next step: we will connect tax knowledge base")
